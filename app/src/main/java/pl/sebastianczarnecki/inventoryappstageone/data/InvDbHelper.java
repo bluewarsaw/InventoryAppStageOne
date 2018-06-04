d377a410b1f297703b2af6b0e3291d4963efb859
Sebastian Czarnecki <32302475+bluewarsaw@users.noreply.github.com>
2018-06-04 11:36:17 +0200

    bug fix

--- app/src/main/java/pl/sebastianczarnecki/inventoryappstageone/data/InvDbHelper.java
+++ app/src/main/java/pl/sebastianczarnecki/inventoryappstageone/data/InvDbHelper.java
@@ -20,12 +20,12 @@ public class InvDbHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         String SQL_CREATE_PRODUCT_TABLE = "CREATE TABLE " + InvContract.InventoryEntry.TABLE_NAME + "("
-                + InvContract.InventoryEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + InvContract.InventoryEntry.COLUMN_NAME + "TEXT NOT NULL,"
-                + InvContract.InventoryEntry.COLUMN_PRODUCT + "INTEGER NOT NULL,"
-                + InvContract.InventoryEntry.COLUMN_QUANTITY + "INTEGER NOT NULL,"
-                + InvContract.InventoryEntry.COLUMN_SUPPLIER_NAME + "INTEGER NOT NULL DEFAULT 0,"
-                + InvContract.InventoryEntry.COLUMN_SUPPLIER_PHONE + "INTEGER);";
+                + InvContract.InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + InvContract.InventoryEntry.COLUMN_NAME + " TEXT NOT NULL, "
+                + InvContract.InventoryEntry.COLUMN_PRODUCT + " INTEGER NOT NULL, "
+                + InvContract.InventoryEntry.COLUMN_QUANTITY + " INTEGER NOT NULL, "
+                + InvContract.InventoryEntry.COLUMN_SUPPLIER_NAME + " INTEGER NOT NULL DEFAULT 0, "
+                + InvContract.InventoryEntry.COLUMN_SUPPLIER_PHONE + " INTEGER);" ;
 
         db.execSQL(SQL_CREATE_PRODUCT_TABLE);
 
